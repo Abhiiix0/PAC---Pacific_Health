bd486eff43290d4c03010d518655102eccea328a
Abhishek Nayak <abhishek.nayak7766@gmail.com>
2024-12-23 22:39:53 +0530

    performance fix

--- js/script.js
+++ js/script.js
@@ -78,22 +78,22 @@ if (typeof Swiper !== "undefined") {
     },
   });
 }
-let images = document.querySelectorAll(".slider-img");
+let images = document?.querySelectorAll(".slider-img");
 let currentIndex = 0;
 
 function changeImage() {
   // Remove the active class from the current image
-  images[currentIndex].classList.remove("active");
+  images[currentIndex]?.classList?.remove("active");
 
   // Update the index for the next image
-  currentIndex = (currentIndex + 1) % images.length;
+  currentIndex = (currentIndex + 1) % images?.length;
 
   // Add the active class to the new image
-  images[currentIndex].classList.add("active");
+  images[currentIndex]?.classList?.add("active");
 }
 
 // Start the automatic image change every 3 seconds
 setInterval(changeImage, 3000);
 
 // Initially show the first image
-images[currentIndex].classList.add("active");
+images[currentIndex]?.classList?.add("active");
